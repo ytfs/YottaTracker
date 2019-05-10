43ea18b1a4eead6ee3f849359a13f65125bd53f3
sdumaoziqi <15866605371@163.com>
2019-05-10 19:02:14 +0800

    get YTA token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'Yotta Tracker',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'https://proxy.eosnode.tools',
+  blockchainUrl: 'http://152.136.11.202:8888',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 };
