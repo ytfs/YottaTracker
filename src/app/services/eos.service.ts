2c3c4ae7d611a6d6f7e19291996c9f2e5f4a30d4
sdumaoziqi <15866605371@163.com>
2019-05-12 12:19:31 +0800

    update

--- src/app/services/eos.service.ts
+++ src/app/services/eos.service.ts
@@ -85,7 +85,7 @@ export class EosService {
   }
 
   getAccountTokens(name: string): Observable<Result<any[]>> {
-    const allTokens$: Observable<any[]> = this.http.get<any[]>(`https://raw.githubusercontent.com/sdumaoziqi/YottaTokens/master/tokens.json`);
+    const allTokens$: Observable<any[]> = this.http.get<any[]>(`https://raw.githubusercontent.com/ytfs/YottaTokens/master/tokens.json`);
     const getCurrencyBalance = function (token: any, account: string): Observable<any> {
       return from(this.eos.getCurrencyBalance(token.account, account, token.symbol)).pipe(
         map((balance: string[]) => ({
